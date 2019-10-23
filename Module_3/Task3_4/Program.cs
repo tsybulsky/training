b7c43ee37a5ea9367e55afa0cd7c91bf3fafcf9c
tsybulsky <gmishka@tut.by>
2019-10-23 09:38:11 +0300

    Module 3. All tasks

---
+++ Module_3/Task3_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Module3_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число: ");
+            var inputValue = Console.ReadLine();
+            inputValue = inputValue.Trim();
+            if (!double.TryParse(inputValue, out _))
+            {
+                Console.WriteLine("Неверное число");
+                Console.ReadKey();
+                return;
+            }
+            var data = inputValue.ToCharArray();            
+            var j = data.Length-1;
+            var i = (char.IsDigit(data[0]))?0:1;
+            while (i < j)
+            {
+                if (!char.IsDigit(data[i]))
+                    i++;
+                if (!char.IsDigit(data[j]))
+                    j--;
+                if (i < j)
+                {
+                    var temp = data[i];
+                    data[i++] = data[j];
+                    data[j--] = temp;
+                }
+            }
+            Console.WriteLine(new String(data));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module_3/Task3_4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Module3_4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Module3_4")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("17bef53b-fd46-463b-9a66-88461af4a23e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
