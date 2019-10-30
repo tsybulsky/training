c4955d1e6a109e58710f51f9dd7bcf575657d03a
tsybulsky <gmishka@tut.by>
2019-10-30 18:38:27 +0300

    Module 4 All tasks

---
+++ Module_4/Task4_2/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_2
+{
+    class Program
+    {
+
+        static int AddThreeIntegers(int a, int b, int c)
+        {
+            return (a + b + c);
+        }
+
+        static int AddTwoIntegers(int a, int b)
+        {
+            return (a + b);
+        }
+
+        static double AddThreeDoubles(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        static string ConcatTwoStrings(string s1, string s2)
+        {
+            return s1 + s2;
+        }
+
+        static int[] AddArrays(int[] a, int[] b)
+        {
+            int Len1 = a.Length;
+            int Len2 = b.Length;
+            int Len = Math.Max(Len1, Len2);
+            int[] result = new int[Len];
+            for (var i = 0; i < Len; i++)
+            {
+                if (i < Len1)
+                {
+                    if (i < Len2)
+                        result[i] = a[i] + b[i];
+                    else
+                        result[i] = a[i];
+                }
+                else if (i < Len2)
+                {
+                    result[i] = b[i];
+                }
+            }
+            return result;
+        }
+        
+        static void Main()
+        {
+            Console.Write("Введите 3 целых числа: ");
+            var inputValue = Console.ReadLine();
+            string[] values = inputValue.Split(' ');
+            if (values.Length < 3)
+            {
+                Console.WriteLine("Введено слишком мало чисел");
+                Console.ReadKey();
+                return;
+            }
+            int[] numbers = new int[3];
+            for (var i = 0; i < 3; i++)
+            {
+                if (!int.TryParse(values[i], out numbers[i]))
+                {
+                    Console.WriteLine($"'{values[i]}' не является целым числом");
+                    Console.ReadKey();
+                    return;
+                }
+            }            
+            int result = AddThreeIntegers(numbers[0], numbers[1], numbers[2]);
+            Console.WriteLine($"AddThreeIntegers({numbers[0]}, {numbers[1]}, {numbers[2]}) = {result}");
+            result = AddTwoIntegers(numbers[0], numbers[1]);
+            Console.WriteLine($"AddTwoIntegers({numbers[0]}, {numbers[1]}) = {result}");
+            Console.Write("Введите 3 числа: ");
+            inputValue = Console.ReadLine();
+            values = inputValue.Split(' ');
+            if (values.Length < 3)
+            {
+                Console.WriteLine("Введено слишком мало чисел");
+                Console.ReadKey();
+                return;
+            }
+            double[] numbers2 = new double[3];
+            for (var i = 0; i < 3; i++)
+            {
+                if (!double.TryParse(values[i], out numbers2[i]))
+                {
+                    Console.WriteLine($"'{values[i]}' не является числом");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            double result2 = AddThreeDoubles(numbers2[0], numbers2[1], numbers[2]);
+            Console.WriteLine($"AddThreeDoubles({numbers2[0]:N3}, {numbers2[1]:N3}, {numbers2[3]:N3})={result2:N3}");
+            Console.Write("Введите первую строку: ");
+            var line1 = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            var line2 = Console.ReadLine();
+            Console.WriteLine($"ConcatTwoStrings(\"{line1}\", \"{line2}\")=\"{ConcatTwoStrings(line1,line2)}\"");
+            Console.Write("Введите размер первого массива (1..20): ");
+            inputValue = Console.ReadLine().Trim();            
+            if ((!int.TryParse(inputValue, out int Len1)) || (Len1 <= 0) || (Len1 > 20))
+            {
+                Console.WriteLine("Неверная длина массива");
+                Console.ReadKey();
+                return;
+            }
+            var rand = new Random();
+            int[] array1 = new int[Len1];
+            for (var i = 0; i < Len1; i++)
+            {
+                array1[i] = rand.Next(-100, 100);
+            }
+            Console.Write("Введите размер второго массива (1..20): ");
+            inputValue = Console.ReadLine().Trim();
+            if ((!int.TryParse(inputValue, out int Len2)) || (Len2 <= 0) || (Len2 > 20))
+            {
+                Console.WriteLine("Неверная длина массива");
+                Console.ReadKey();
+                return;
+            }
+            int[] array2 = new int[Len2];
+            for (var i = 0; i < Len2; i++)
+            {
+                array2[i] = rand.Next(-100, 100);
+            }
+            Console.Write("Первый массив:");
+            for (var i = 0; i < Len1; i++)
+            {
+                Console.Write($" {array1[i]}");
+            }
+            Console.WriteLine("");
+            Console.Write("Второй массив:");
+            for (var i = 0; i < Len2; i++)
+            {
+                Console.Write($" {array2[i]}");
+            }
+            Console.WriteLine("");
+            int[] resultArray = AddArrays(array1, array2);
+            Console.Write("Результирующий массив:");
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write($" {resultArray[i]}");
+            }
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module_4/Task4_3/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_3
+{
+    class Program
+    {
+        static void IncreaseVariables(ref int var1, ref int var2, ref int var3)
+        {
+            var1 += 10;
+            var2 += 10;
+            var3 += 10;
+        }
+
+        static void CalculateCircleParams(double r, out double p, out double s)
+        {
+            p = 2 * Math.PI * r;
+            s = Math.PI * r * r;
+        }
+
+        static void GetArrayMinMaxAndSum(int[] array, out int max, out int min, out int sum)
+        {            
+            min = int.MaxValue;
+            max = int.MinValue;
+            sum = 0;
+            foreach (var item in array)
+            {
+                if (item > max)
+                    max = item;
+                if (item < min)
+                    min = item;
+                sum += item;
+            }
+        }
+
+        static void Main()
+        {
+            Console.Write("Введите 3 целых числа: ");
+            var inputValue = Console.ReadLine();
+            string[] values = inputValue.Split(' ');
+            if (values.Length < 3)
+            {
+                Console.WriteLine("Введено слишком мало чисел");
+                Console.ReadKey();
+                return;
+            }
+            int[] numbers = new int[3];
+            for (var i = 0; i < 3; i++)
+            {
+                if (!int.TryParse(values[i], out numbers[i]))
+                {
+                    Console.WriteLine($"'{values[i]}' не является целым числом");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            IncreaseVariables(ref numbers[0], ref numbers[1], ref numbers[2]);                                    
+            Console.WriteLine($"Увеличенные переменные: {numbers[0]}, {numbers[1]}, c={numbers[2]}");
+            Console.Write("Введите радиус круга: ");
+            inputValue = Console.ReadLine();            
+            if ((!double.TryParse(inputValue, out double r)) || (r <= 0))
+            {
+                Console.WriteLine("Неверный радиус круга");
+                Console.ReadKey();
+                return;
+            }            
+            CalculateCircleParams(r, out double p, out double s);
+            Console.WriteLine($"Для радиуса r={r:N3}, периметр равен P={p:N3}, а площадь круга S={s:N3}");
+            Console.Write("Введите размер массива (1..20): ");
+            inputValue = Console.ReadLine();            
+            if ((!int.TryParse(inputValue, out int len)) || (len <= 0) || (len > 20))
+            {
+                Console.WriteLine("Неверная длина массива");
+                Console.ReadKey();
+                return;
+            }
+            var rand = new Random();
+            int[] array = new int[len];
+            for (var i = 0; i < len; i++)
+            {
+                array[i] = rand.Next(-100, 100);
+            }
+            Console.Write("Исходный массив:");
+            foreach (var item in array)
+            {
+                Console.Write($" {item}");
+            }            
+            GetArrayMinMaxAndSum(array, out int min, out int max, out int sum);
+            Console.WriteLine("");
+            Console.WriteLine($"Минимальный элемент: {min}, максимальный элемент: {max}, сумма элементов: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
